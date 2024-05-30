90322313a1e2bd1d270d62c7cf0ce774b2933e19
Sandesh7969 <155588072+Sandesh7969@users.noreply.github.com>
2024-05-30 18:09:35 +0545

    Update Config.js

--- build/src/setting/Config.js
+++ build/src/setting/Config.js
@@ -17,54 +17,54 @@ export default class OrasConfig extends Object {
     constructor() {
         super();
         this.token =
-            "";
-        this.botid = ""
-        this.prefix = ".";
+            "" || process.env['token'];
+        this.botid = "1180892789840547840"
+        this.prefix = "*";
         this.nodes = [
             {
                 name: `Kronix`,
-                url: `lava.link:80`,
-                auth: `kronix`,
+                url: `buses.sleepyinsomniac.eu.org`,
+                auth: `youshallnotpass`,
                 secure: false,
             },
           {
               name: `Oras`,
-              url: `lava.link:80`,
-              auth: `kronix`,
+              url: `ll.sleepyinsomniac.eu.org`,
+              auth: `youshallnotpass`,
               secure: false,
           }, // Dont Change The Name Of Nodes
         ];
         this.voteApi =
             "";
         this.webhooks = {
-            guildCreate: "",
-            guildDelete: "",
-            Cmds: "",
+            guildCreate: "" || process.env['guildCreate'],
+            guildDelete: "" || process.env['guildDelete'],
+            Cmds: "" || process.env['cmds'],
         };
-        this.server = "https://discord.gg/aMC2e8zgQb";
-        this.botinvite = `https://discord.com/api/oauth2/authorize?client_id=1106136153662435348&permissions=8&scope=applications.commands%20bot`;
-        this.spotiId = "";
-        this.spotiSecret = "";
-        this.owners = ["1057674644905279498"];
+        this.server = "https://discord.gg/GPzYFx7zfe";
+        this.botinvite = `https://discord.com/api/oauth2/authorize?client_id=1180892789840547840&permissions=8&scope=applications.commands%20bot`;
+        this.spotiId = "6c31645ffb004ab8b44d06f7b96d1b66";
+        this.spotiSecret = "3618fdc0b4824cfd91a8d425dac32987";
+        this.owners = ["1131953433134497923"];
         this.color = "#45FFCA";
-        this.supportId = "1124739194208661514"; //Server Id
+        this.supportId = ""; //Server Id
         this.spotiNodes = [
             {
                 id: `Kronix`,
-                host: `lava.link`,
-                port: 80,
-                password: `kronix`,
-                secure: false,
+                host: `ll.sleepyinsomniac.eu.org`,
+                port: 443,
+                password: `youshallnotpass`,
+                secure: true,
             },
           {
               id: `Oras`,
-              host: `lava.link`,
+              host: `buses.sleepyinsomniac.eu.org`,
               port: 80,
-              password: `kronix`,
+              password: `youshallnotpass`,
               secure: false,
           }, // Dont Change The Name Of Nodes
         ];
-        this.voteUrl = "https://top.gg/bot/1027828697828433980/vote";
+        this.voteUrl = "https://discord.gg/GPzYFx7zfe";
         this.setupBgLink =
             "https://media.discordapp.net/attachments/1190943041922535517/1190943144158691369/live_now.png?ex=65a3a33c&is=65912e3c&hm=3f442c1bd2f8b61e3be1f1c625d891df2b90334098765ca174750746912df20d&=&format=webp&quality=lossless&width=1000&height=562";
     }
